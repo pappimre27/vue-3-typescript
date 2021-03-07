79846149981c9e28d025f1cfb90395ed53fb5f6f
Papp Imre <42896970+pappimre27@users.noreply.github.com>
2021-03-07 13:20:35 +0100

    Add files via upload

---
+++ src/views/EventList.vue
@@ -0,0 +1,58 @@
+<template>
+<div>
+    <h1>Events for Good</h1>
+    <div class="events">
+      <EventCard v-for="event in events" :key="event.id" :event="event" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+// eslint-disable-next-line no-unused-vars
+import { EventItem } from '../types'
+import EventCard from '../components/EventCard.vue'
+import EventService from '../services/EventService'
+
+export default defineComponent({
+  name: 'EventList',
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: [] as EventItem[]
+    }
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  },
+  computed: {
+    firstEvent(): EventItem {
+      return this.events[0]
+    }
+  },
+  methods: {
+    addEvent(newEvent: EventItem): void {
+      this.events.push(newEvent)
+    },
+    secondEvent(): EventItem {
+      return this.events[1] 
+    }
+  }
+})
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
